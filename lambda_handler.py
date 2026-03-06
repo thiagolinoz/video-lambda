713467ec5070066a6529b121bb937ff22c72592f
maycon-alves <72166726+maycon-alves@users.noreply.github.com>
2026-03-05 22:38:26 -0300

    adicionando validação police api gateway

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,24 +1,88 @@
 
 import boto3
+import base64
+
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('Usuarios')
+table = dynamodb.Table('Persons')
 
 def lambda_handler(event, context):
-    email = event.get("email")
-    senha = event.get("senha")
+
+    method_arn = event["methodArn"]
+
+    headers = {k.lower(): v for k, v in event["headers"].items()}
+    auth_header = headers.get("authorization")
+    print ("Authorization header:", auth_header)
+    if auth_header and auth_header.startswith("Basic "):
+
+        encoded_credentials = auth_header.split(" ")[1]
+
+        decoded = base64.b64decode(encoded_credentials).decode("utf-8")
+
+        email, senha = decoded.split(":")
+
+
 
     if not email or not senha:
-        return {"statusCode": 400, "body": "Email e senha obrigatórios"}
+        print ('{"statusCode": 400, "body": "Email e senha obrigatórios"}')
+        effect = "Deny"
 
     response = table.get_item(Key={"email": email})
 
     if "Item" not in response:
-        return {"statusCode": 404, "body": "Usuário não encontrado"}
+        print('{"statusCode": 404, "body": "Usuário não encontrado"}')
+        effect = "Deny"
 
-    senha_banco = response["Item"]["senha"]
+    senha_banco = response["Item"]["cdPassword"]
 
     if senha_banco == senha:
-        return {"statusCode": 200, "body": "Login autorizado"}
+        print('{"statusCode": 200, "body": "Login autorizado"}')
+        effect = "Allow"
     else:
-        return {"statusCode": 401, "body": "Senha inválida"}
+        print('{"statusCode": 401, "body": "Senha inválida"}')
+        effect = "Deny"
+
+    return {
+        "principalId": "test-user",
+        "policyDocument": {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Action": "execute-api:Invoke",
+                    "Effect": effect,
+                    "Resource": method_arn
+                }
+            ]
+        }
+    }
+##################################
+########### TESTE BASE ###########
+##################################
+
+# import base64
+
+# def lambda_handler(event, context):
+
+#     method_arn = event["methodArn"]
+
+#     headers = {k.lower(): v for k, v in event["headers"].items()}
+#     auth_header = headers.get("authorization")
+#     print ("Authorization header:", auth_header)
+#     if auth_header and auth_header.startswith("Basic "):
+
+#         encoded_credentials = auth_header.split(" ")[1]
+
+#         decoded = base64.b64decode(encoded_credentials).decode("utf-8")
+
+#         email, senha = decoded.split(":")
+
+#         if (email == "certo") and (senha == "certa"):
+#             print("Access granted")
+#             effect = "Allow"
+#         else:
+#             effect = "Deny"
+
+#     else:
+#         effect = "Deny"
+
+    
